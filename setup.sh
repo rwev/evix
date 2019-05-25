34468bcdb639400fecaa0fbbe3e44933975250c8
rwev <rwev@protonmail.ch>
2019-05-25 11:02:44 -0700

    pantheon terminal settings; default tmux shell to zsh

--- setup.sh
+++ setup.sh
@@ -25,9 +25,18 @@ curl -fLo ~/.vim/colors/nord.vim --create-dirs https://raw.githubusercontent.com
 curl -fLo ./Source\ Code\ Pro\ Medium\ for\ Powerline.otf --create-dirs
 https://github.com/powerline/fonts/blob/master/SourceCodePro/Source%20Code%20Pro%20Medium%20for%20Powerline.otf
 mv ./Source\ Code\ Pro\ Medium\ for\ Powerline.otf ~/.local/share/fonts/type1
-gsettings set org.pantheon.terminal.settings font 'Source Code Pro for Powerline
-Medium 14'
 sudo fc-cache -fv # fc-list | grep "Source Code Pro" to confirm
+
+# pantheon terminal
+gsettings set org.pantheon.terminal.settings font 'Source Code Pro for Powerline Regular 16'
+gsettings set  org.pantheon.terminal.settings foreground '#D8DEE9'
+gsettings set  org.pantheon.terminal.settings background '#2E3440'
+gsettings set  org.pantheon.terminal.settings palette '#3B4252:#BF616A:#A3BE8C:#EBCB8B:#81A1C1:#B48EAD:#88C0D0:#E5E9F0:#4C566A:#BF616A:#A3BE8C:#EBCB8B:#81A1C1:#B48EAD:#8FBCBB:#ECEFF4'
+gsettings set org.pantheon.terminal.settings tab-bar-behavior 'Never Show Tabs'
+gsettings set org.pantheon.terminal.settings remember-tabs false
+gsettings set org.pantheon.terminal.settings save-exited-tabs false
+gsettings set org.pantheon.terminal.settings alt-changes-tab false
+gsettings set org.pantheon.terminal.settings follow-last-tab false
 
 # X*
 curl -fLo ./nord-xresources --create-dirs https://raw.githubusercontent.com/arcticicestudio/nord-xresources/develop/src/nord
